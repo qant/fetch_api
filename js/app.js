1c46ad6a6cf907ccb8ab81269180fae496c0632a
Anton Semenov <anseme@gmail.com>
2020-01-04 10:21:45 +0100

    Catch err, fetch json data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
+//Events
 const loadTextBtn = document
   .getElementById("loadText")
   .addEventListener("click", loadText);
 const loadJsonBtn = document
   .getElementById("loadJson")
-  .addEventListener("click", loadText);
+  .addEventListener("click", loadJson);
 const loadApiBtn = document
   .getElementById("loadApi")
   .addEventListener("click", loadText);
+
+//Loaders
+function loadJson() {
+  fetch("empleados.json")
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      showJSON(data);
+    })
+    .catch(err => {
+      getError(err);
+    });
+}
 
 function loadText() {
   fetch("datos.txt")
@@ -20,5 +35,25 @@ function loadText() {
       //data from promise
       console.log(data);
       document.getElementById("result").innerHTML = data;
+    })
+    .catch(function(err) {
+      //if fech fail
+      console.error(err);
     });
+}
+
+//Helpers
+
+function getError(err) {
+  console.error(err);
+}
+
+function showJSON(data) {
+  console.log(data);
+  let html = "";
+  data.forEach(item => {
+    html += `<li>Name:${item.nombre}</li>`;
+    html += `<li>Position:${item.puesto}</li><hr>`;
+  });
+  document.getElementById("result").innerHTML = html;
 }
